9830eb31a330e1bc2eb1b516aa0e601bd2bb2838
Jirka-Mayer <jirkamayer97@gmail.com>
2019-03-24 22:27:22 +0100

    Gaussian elimination

---
+++ Benchmarks/GaussianElimination.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Cuni.Arithmetics.FixedPoint;
+
+namespace Benchmarks
+{
+    public static class GaussianElimination
+    {
+        public static Fixed<Q>[,] RandomMatrix<Q>(int n) where Q : IQ, new()
+        {
+            Random r = new Random();
+            var mat = new Fixed<Q>[n, n];
+
+            for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                mat[i, j] = new Fixed<Q>(r.Next());
+
+            return mat;
+
+            // mat[row, col]
+        }
+
+        public static void Run<Q>(int n, Fixed<Q>[,] mat) where Q : IQ, new()
+        {
+            for (int col = 0; col < n - 1; col++)
+            {
+                for (int row = col + 1; row < n; row++)
+                {
+                    // null a single cell
+                    Fixed<Q> alpha = mat[col, row] / mat[col, col];
+                    for (int c = 0; c < n; c++)
+                        mat[c, row] -= mat[c, col] * alpha;
+                }
+            }
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Cuni.Arithmetics.FixedPoint;
+
+namespace Benchmarks
+{
+    public class BenchmarkSuite
+    {
+        private Fixed<Q16_16>[,] mat;
+        private int N = 10;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            mat = GaussianElimination.RandomMatrix<Q16_16>(N);
+        }
+
+        [Benchmark]
+        public void Elimination() => GaussianElimination.Run(N, mat);
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BenchmarkSuite>();
+        }
+    }
+}
